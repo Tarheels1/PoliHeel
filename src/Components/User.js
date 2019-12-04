a0b8a2804e607291c14e2b0525d51f27be770693
Tarheels1 <moreno.eric01@gmail.com>
2019-12-03 21:16:18 -0500

    Added Rep Comp & Updated UserPage

--- src/Components/User.js
+++ src/Components/User.js
@@ -3,6 +3,8 @@ import app from "../config/base.js";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
+import Typography from "material-ui/styles/typography";
+import axios from "axios";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -14,6 +16,11 @@ const useStyles = makeStyles(theme => ({
     color: theme.palette.text.secondary
   }
 }));
+
+const headingStyle = {
+  textAlign: "left",
+  padding: "30px"
+};
 
 const User = () => {
   const classes = useStyles();
@@ -38,10 +45,29 @@ const User = () => {
   return (
     <div>
       <h1>Welcome {userInfo.FirstName}!</h1>
-      <div>
-        <h2></h2>
+      <subtitle>
+        Listed below are your Federal and State Congressional Members
+      </subtitle>
+      <div></div>
+
+      <div className="federalSenators">
+        <h2 style={headingStyle}>Federal Senators</h2>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>
+            edkjsa;ajkda;j;kaf<br></br> dsa;ljfa;dsjfas kj;fdas;
+          </Paper>
+        </Grid>
       </div>
-      <h2></h2>
+      <div className="federalRep">
+        <h2 style={headingStyle}>Federal Represenative</h2>
+      </div>
+      <div className="stateSenators">
+        <h2 style={headingStyle}>State Senators</h2>
+      </div>
+      <div className="stateRep">
+        <h2 style={headingStyle}>State Representative</h2>
+      </div>
+
       <button onClick={() => app.auth().signOut()}>Sign Out</button>
     </div>
   );
